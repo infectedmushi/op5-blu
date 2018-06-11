2ed2f30f058e923874f105856582326e5aec139e
Sultan Alsawaf <sultanxda@gmail.com>
2018-06-11 16:19:54 +0100

    add build script

---
+++ build-anykernel.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+
+#
+#  Build Script for RenderZenith for OnePlus 5!
+#  Based off AK'sbuild script - Thanks!
+#
+
+# Bash Color
+rm .version
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Resources
+THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
+KERNEL="Image.gz-dtb"
+DEFCONFIG="blu_spark_defconfig"
+
+# Kernel Details
+VER=RenderZenith
+VARIANT="OP5T"
+
+# Kernel zip name
+HASH=`git rev-parse --short=8 HEAD`
+KERNEL_ZIP="BM-$VARIANT-$(date +%y%m%d)-$HASH" 
+
+# Vars
+export LOCALVERSION=~`echo $VER`
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER=infected_
+export KBUILD_BUILD_HOST=infected-labs
+export LOCALVERSION=~`echo $KERNEL_ZIP`
+export CCACHE=ccache
+
+# Paths
+KERNEL_DIR="/mnt/Building/blu_spark-mod"
+KBUILD_OUTPUT="${KERNEL_DIR}/../out"
+REPACK_DIR="/mnt/Building/AnyKernel2-blu_spark"
+PATCH_DIR="/mnt/Building/AnyKernel2-blu_spark/patch"
+MODULES_DIR="/mnt/Building/AnyKernel2-blu_spark/modules/system/lib/modules"
+ZIP_MOVE="/mnt/Building/Out_Zips"
+ZIMAGE_DIR="$KBUILD_OUTPUT/arch/arm64/boot"
+
+# Functions
+function checkout_ak2_branches {
+        cd $REPACK_DIR
+        git checkout rk-op5-oos-o
+        cd $KERNEL_DIR
+}
+
+function clean_all {
+        cd $REPACK_DIR
+        rm -rf $MODULES_DIR/*
+        rm -rf $KERNEL
+        rm -rf $DTBIMAGE
+        rm -rf zImage
+        cd $KERNEL_DIR
+        echo
+        make O=${KBUILD_OUTPUT} clean && make O=${KBUILD_OUTPUT} mrproper
+}
+
+function make_kernel {
+        echo
+        make O=${KBUILD_OUTPUT} $DEFCONFIG
+        make O=${KBUILD_OUTPUT} $THREAD
+}
+
+function make_modules {
+	# Remove and re-create modules directory
+	rm -rf $MODULES_DIR
+	mkdir -p $MODULES_DIR
+
+	# Copy modules over
+	echo ""
+        find $KBUILD_OUTPUT -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+
+	# Strip modules
+	${CROSS_COMPILE}strip --strip-unneeded $MODULES_DIR/*.ko
+
+	# Sign modules
+	find $MODULES_DIR -name '*.ko' -exec $KBUILD_OUTPUT/scripts/sign-file sha512 $KBUILD_OUTPUT/certs/signing_key.pem $KBUILD_OUTPUT/certs/signing_key.x509 {} \;
+}
+
+function make_zip {
+        cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR/$KERNEL
+        cd $REPACK_DIR
+        zip -r9 $KERNEL_ZIP.zip * 
+        mv $KERNEL_ZIP.zip $ZIP_MOVE
+        cd $KERNEL_DIR
+}
+
+DATE_START=$(date +"%s")
+
+echo -e "${green}"
+echo "Stock+ creation script:"
+echo -e "${restore}"
+
+echo "Pick Toolchain..."
+select choice in gcc-linaro-6.4.1-2018.05-x86_64_aarch64-linux-gnu gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu gcc-arm-8.2-2018.08-x86_64-aarch64-linux-gnu
+do
+case "$choice" in
+    "gcc-linaro-6.4.1-2018.05-x86_64_aarch64-linux-gnu")
+        export CROSS_COMPILE=${HOME}/android/source/toolchains/gcc-linaro-6.4.1-2018.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+        break;;
+    "gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu")
+        export CROSS_COMPILE=/mnt/Linux/toolchains/aarch64-linux-gnu-7.3.1-linaro/bin/aarch64-linux-gnu-
+        break;;
+    "gcc-arm-8.2-2018.08-x86_64-aarch64-linux-gnu")
+        export CROSS_COMPILE=/mnt/Linux/toolchains/gcc-arm-8.2-2018.08-x86_64-aarch64-linux-gnu/bin/aarch64-linux-gnu-
+        break;;
+
+esac
+done
+
+# Use CCACHE
+export CROSS_COMPILE="${CCACHE} ${CROSS_COMPILE}"
+
+while read -p "Do you want to clean stuffs (y/n)? " cchoice
+do
+case "$cchoice" in
+    y|Y )
+        #checkout_ak2_branches
+        clean_all
+        echo
+        echo "All Cleaned now."
+        break
+        ;;
+    n|N )
+        break
+        ;;
+    * )
+        echo
+        echo "Invalid try again!"
+        echo
+        ;;
+esac
+done
+
+echo
+
+while read -p "Do you want to build kernel (y/n)? " dchoice
+do
+case "$dchoice" in
+  y|Y)
+    make_kernel
+    break
+    ;;
+  n|N )
+    break
+    ;;
+  * )
+    echo
+    echo "Invalid try again!"
+    echo
+    ;;
+esac
+done
+
+while read -p "Do you want to ZIP kernel (y/n)? " dchoice
+do
+case "$dchoice" in
+  y|Y)
+    #make_modules
+    make_zip
+    break
+    ;;
+  n|N )
+    break
+    ;;
+  * )
+    echo
+    echo "Invalid try again!"
+    echo
+    ;;
+esac
+done
+
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
